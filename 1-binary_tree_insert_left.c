8073a67b78ab8b52fc15315c60329084f5847ff0
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-18 14:14:54 -0800

    fixing variable name

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = NULL;
 
-	child = malloc(sizeof(binary_tree_t));
+	newNode = malloc(sizeof(binary_tree_t));
 	if (newNode == NULL || parent == NULL)
 		return (NULL);
 
