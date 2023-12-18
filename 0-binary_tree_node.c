aafe32b7dc42f71d805d81cdf95808e43dcb7f30
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-18 13:55:56 -0800

    changing value to n and fixing typo

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,10 +15,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!newNode)
 		return (NULL);
 
-	newNode->value = value;
+	newNode->n = value;
 	newNode->parent = parent;
 	newNode->left = NULL;
 	newNode->right = NULL;
 
-	return (NewNode);
+	return (newNode);
 }
