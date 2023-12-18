ca071268556cf6ecaa32dc0d024733289c91f397
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-18 14:27:06 -0800

    changing check for if right child already exists

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	newNode->left = NULL;
 	newNode->right = NULL;
 
-	if (newNode->right != NULL)
+	if (parent->right != NULL)
 	{
 		newNode->right = parent->right;
 		newNode->right->parent = newNode;
