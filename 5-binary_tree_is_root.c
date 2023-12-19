ff182102eb47f4dd814da5ff3de8a97afd531165
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-19 12:43:53 -0800

    is node root?

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_is_root - checks if the node is root
  *
- * @node - node to check if root
+ * @node: node to check if root
  * Return: If Root: 1
  * If Not Root: 0
  **/
@@ -10,7 +10,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	
+
 	if (node->parent == NULL)
 		return (1);
 
