4fbfe0fcef28d21178081470b85d18f501815c94
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-20 09:55:31 -0800

    removing whitespace

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,6 +20,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		rightHeight = 1 + binary_tree_height(tree->right);
 	if (leftHeight > rightHeight)
 		return (leftHeight);
-	else 
+	else
 		return (rightHeight);
 }
