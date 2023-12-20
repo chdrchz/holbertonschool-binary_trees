00d04d7c4033ebc6d266f589c5804aa6aa2b9670
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-20 09:49:18 -0800

    another test, change return values

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,11 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t rightHeight = 0;
 	
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	else
 	{
 		if (tree->left == NULL || tree->right == NULL)
-			return (0);
+			return (-1);
 		if (tree->left != NULL)
 			leftHeight = 1 + binary_tree_height(tree->left);
 		if (tree->right != NULL)
