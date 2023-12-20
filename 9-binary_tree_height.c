3d0c4119c2be43e907b831681737f3b98270db00
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-20 08:57:45 -0800

    adding greater than or equal to

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	leftHeight = binary_tree_height(tree->left);
 	rightHeight = binary_tree_height(tree->right);
-	if (leftHeight > rightHeight)
+	if (leftHeight >= rightHeight)
 		return (leftHeight + 1);
 	else
 		return (rightHeight + 1);
