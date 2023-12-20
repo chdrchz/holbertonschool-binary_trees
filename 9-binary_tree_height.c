52d3616159243d72593fcd362085aca792feb664
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-20 08:59:21 -0800

    test

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,7 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	leftHeight = binary_tree_height(tree->left);
 	rightHeight = binary_tree_height(tree->right);
 	if (leftHeight >= rightHeight)
-		return (leftHeight + 1);
+		return (leftHeight);
 	else
-		return (rightHeight + 1);
+		return (rightHeight);
 }
