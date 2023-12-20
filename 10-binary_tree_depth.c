4e73cc4b7fafa455d261474007fdec8ab8d2d724
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-20 12:17:08 -0800

    compile errors

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,19 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - finds depth of tree
- *
- * @tree: tree to find depth of
- * Return: On Success: Depth of tree
- * On Failure: 0
- **/
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (binary_tree_depth_recursive(tree));
-}
-
-/**
  * binary_tree_depth_recursive - recursively finds depth of tree
  *
  * @current: current node to check depth
@@ -25,4 +11,17 @@ size_t binary_tree_depth_recursive(const binary_tree_t *current)
 	if (current == NULL || current->parent == NULL)
 		return (0);
 	return (1 + binary_tree_depth_recursive(current->parent));
+}
+/**
+ * binary_tree_depth - finds depth of tree
+ *
+ * @tree: tree to find depth of
+ * Return: On Success: Depth of tree
+ * On Failure: 0
+ **/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_depth_recursive(tree));
 }
