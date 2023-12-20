f13b444dad96b821ec9813ffc96629da2c86edec
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-20 09:54:38 -0800

    pleasing betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,18 +10,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t leftHeight = 0;
 	size_t rightHeight = 0;
-	
+
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		if (tree->left != NULL)
-			leftHeight = 1 + binary_tree_height(tree->left);
-		if (tree->right != NULL)
-			rightHeight = 1 + binary_tree_height(tree->right);
-		if (leftHeight > rightHeight)
-			return (leftHeight);
-		else 
-			return (rightHeight);
-	}
+
+	if (tree->left != NULL)
+		leftHeight = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		rightHeight = 1 + binary_tree_height(tree->right);
+	if (leftHeight > rightHeight)
+		return (leftHeight);
+	else 
+		return (rightHeight);
 }
